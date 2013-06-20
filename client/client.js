6e84474749761baab4b700f52aa097d3cb6537af
Erik Lundin <erklundin@gmail.com>
2013-06-20 23:46:46 +0200

    Replace unique revision numbers with a simpler solution

--- client/client.js
+++ client/client.js
@@ -2,36 +2,38 @@
   var protocol = window.location.protocol || 'http:'
     , host = window.location.host
     , base = protocol + '//' + host + window.location.pathname
-    , seq = 0
 
-  function tinderbox(state) {
+  function tinderbox(current) {
     var socket = io.connect('ws://' + host + '/')
+      , initiated = false
 
     // handle snapshot updates (this should only happen once; when
     // the connection is established for the first time)
-    socket.on('snapshot', function (revisions) {
-      for (var url in revisions) {
-        trigger(url, revisions[url])
+    socket.on('snapshot', function (revs) {
+      // if we receive a second snapshot command the server must've
+      // reset, which in turns means that all revision numbers
+      // are now invalid
+      if (initiated) {
+        window.location.reload()
       }
+
+      for (var url in revs) {
+        check(url, revs[url])
+      }
+
+      initiated = true
     })
 
     // listen for single resource refreshes
     socket.on('refresh', function (url, rev) {
-      trigger(url, rev)
+      check(url, rev)
     })
 
-    function trigger(url, rev) {
-      if (rev < seq) {
-        // TODO: the server must've restarted (because revision ids
-        //       are being reused) -- force a full reload
-      }
-
-      if ((state[url] || -1) < rev) {
+    function check(url, rev) {
+      if ((current[url] || 0) < rev) {
         refresh(url)
-        state[url] = rev
+        current[url] = rev
       }
-
-      seq++
     }
   }
 
